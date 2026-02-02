ae808a662cd7bcbf6d1d8a2ee99da45400742b79
Collins-Alu <c.wairimu@alustudent.com>
2026-02-02 18:47:17 +0200

    problems

--- Souce_code.py
+++ Souce_code.py
@@ -3,6 +3,13 @@ import html
 
 class data_extractor:
     def __init__(self):
+            #regex for matching an email address
+        self.email_pattern = re.compile r'^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,3}$
+                                         
+        self.phone_number = re.compile r'(^\d{10}$)
+                                        
+        self.credit_card_numbers = re.compile r'^(\d{16}|\d{4}-\d{4}-\d{4}-\d{4})$'
+
+        self.hashtags = re.compile '^#[a-zA-Z0-9_]+$'
+
         
-        #regex for matching an email address
-    
