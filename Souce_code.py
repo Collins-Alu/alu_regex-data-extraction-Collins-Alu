cc6e6f02a1367d02a611dad228f1f0dd9d6cadc0
Collins-Alu <c.wairimu@alustudent.com>
2026-02-02 18:50:45 +0200

    Added paranthesis

--- Souce_code.py
+++ Souce_code.py
@@ -4,12 +4,11 @@ import html
 class data_extractor:
     def __init__(self):
             #regex for matching an email address
-        self.email_pattern = re.compile r'^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,3}$
+        self.email_pattern = re.compile (r'^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,3}$')
                                          
-        self.phone_number = re.compile r'(^\d{10}$)
+        self.phone_number = re.compile (r'\^(^\d{10}$')
                                         
-        self.credit_card_numbers = re.compile r'^(\d{16}|\d{4}-\d{4}-\d{4}-\d{4})$'
+        self.credit_card_numbers = re.compile (r'\^(\d{16}|\d{4}-\d{4}-\d{4}-\d{4})$')
 
-        self.hashtags = re.compile '^#[a-zA-Z0-9_]+$'
+        self.hashtags = re.compile (r'\^#[a-zA-Z0-9_]+$')
 
-        
